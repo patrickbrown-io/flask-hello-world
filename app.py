e3293f8d2dd8b631f0cde1f9fb037ed80ac127d8
Pat <breepatbree@gmail.com>
2025-03-20 09:47:43 -0600

    Adds db_test route and pg instance

--- app.py
+++ app.py
@@ -5,8 +5,31 @@
 
 # An index to practice deploying 
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Patrick Brown in 3308'
+@app.route('/db_test')
+def testing():
+    # Db connection
+    conn = psycopg2.connect("postgresql://lab_ten_user:l9Gk7iZ41U78nL2cJtteABJpHhBJrZfw@dpg-cve3e1l2ng1s73ccuqh0-a/lab_ten")
+    # Cursor
+    cur = conn.cursor()
+    # Create our table
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    # Commit
+    conn.commit()
+
+    conn.close() # end with this to close connection
+    return 'Basketball table created!'
