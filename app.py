2ec17262ce75d02012e7745d7ac25b900c140295
Pat <breepatbree@gmail.com>
2025-03-20 09:38:30 -0600

    Edit app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+# Patrick Brown
+# 3/20/25
+# pabr5825 || patrickbrown-io
+
+
+# An index to practice deploying 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Patrick Brown in 3308'
